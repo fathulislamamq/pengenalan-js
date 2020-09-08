825157559b777bfeaeed5a5d9e76b353218e37f6
fathul islam <fathulislamamq@gmail.com>
2020-09-08 09:22:35 +0700

    add project

---
+++ fathul.js
@@ -0,0 +1,11 @@
+// 2. segitiga
+
+var t = 12
+var a = 10
+
+var l = 0.5 * a * t
+var br = "<br>"
+
+document.write("diketahui t " + t + br)
+document.write("diketahui a " + a + br)
+document.write("luas segitiga adalah " + l + br)
